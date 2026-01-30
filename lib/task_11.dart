f61665ec514bc650c503ecc0a950f4cca5478ccf
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-01-30 22:13:31 +0500

    List turi uchun extension type hosil qiling va unda berilgan listni chop qiladigan console nomli method yozing.

---
+++ lib/task_11.dart
@@ -0,0 +1,14 @@
+extension type ConsoleList<T>(List<T> value) {
+  void console() {
+    print(value);
+  }
+}
+
+
+void main() {
+  final numbers = ConsoleList([1, 2, 3, 4]);
+  numbers.console();
+
+  final words = ConsoleList(["a", "b", "c"]);
+  words.console();
+}
